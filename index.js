187864e270d665e6d63d3eb627911ff42befcdae
samstrathman <117926965+samstrathman@users.noreply.github.com>
2023-02-10 16:31:35 -0500

    Began building header

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+const para = document.createElement("p");
+const node = document.createTextNode("This is new.");
+para.appendChild(node);
+
+const element = document.getElementById("div1");
+element.appendChild(para);
+
+console.log("hi");
+*/
