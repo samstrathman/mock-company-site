0c2c3be55da201506fec0e00d68cb7e5563c2eed
samstrathman <117926965+samstrathman@users.noreply.github.com>
2023-02-15 17:08:49 -0500

    added tons of animations!

---
+++ index.js
@@ -0,0 +1,50 @@
+//Stuff for scroll animation
+window.addEventListener(
+    "scroll",
+    () => {
+      document.body.style.setProperty(
+        "--scroll",
+        window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+      );
+    },
+    false
+  );
+
+
+//Stuff to trigger animation when it's in view
+// get the element to animate
+var element = document.getElementById('ten');
+var elementHeight = element.clientHeight;
+
+// listen for scroll event and call animate function
+document.addEventListener('scroll', animate);
+
+// check if element is in view
+function inView() {
+    // get window height
+    var windowHeight = window.innerHeight;
+    // get number of pixels that the document is scrolled
+    var scrollY = window.scrollY || window.pageYOffset;
+  
+    // get current scroll position (distance from the top of the page to the bottom of the current viewport)
+    var scrollPosition = scrollY + windowHeight;
+    // get element position (distance from the top of the page to the bottom of the element)
+    var elementPosition = element.getBoundingClientRect().top + scrollY + elementHeight;
+  
+    // is scroll position greater than element position? (is element in view?)
+    if (scrollPosition > elementPosition) {
+        return true;
+    }
+  
+    return false;
+}
+
+// animate element when it is in view
+function animate() {
+    // is element in view?
+    if (inView()) {
+        // element is in view, add class to element
+        element.classList.add('elongate');
+    }
+}
+
--- slideData.js
+++ slideData.js
@@ -1,26 +1,35 @@
 const data = [
     {
+        'id': 1,
         'img': './assets/company1.jpg',
-        'name': 'This Company' 
+        'name': 'This Company', 
+        'desc': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla facilisi nullam vehicula ipsum a. Velit ut tortor pretium viverra suspendisse potenti nullam.'
     },
     {
+        'id': 2,
         'img': './assets/company2.jpg',
-        'name': 'That Company' 
+        'name': 'That Company', 
+        'desc': 'Justo donec enim diam vulputate. Maecenas sed enim ut sem viverra aliquet eget sit.'
     },
     {
+        'id': 3,
         'img': './assets/company3.jpg',
-        'name': 'Another Company' 
+        'name': 'Another Company',
+        'desc': 'Cursus turpis massa tincidunt dui ut. Accumsan tortor posuere ac ut.' 
     },
     {
+        'id': 4,
         'img': './assets/company4.jpg',
-        'name': 'Best Company' 
+        'name': 'Best Company',
+        'desc': 'Ut porttitor leo a diam sollicitudin tempor id eu nisl. Vivamus arcu felis bibendum ut. Dictum sit amet justo donec enim. Viverra nam libero justo laoreet sit.' 
     }
 ];
 
+//create all the img elements and give them a classname that will keep them hidden
 data.map((i) => {
     let img = document.createElement('img');
     img.src = i.img;
-    img.className = 'slideshow-img';
+    img.className = 'slideshow-img fade';
     document.getElementById('slide-container').appendChild(img);
 })
 
@@ -32,36 +41,22 @@ function plusDivs(n) {
 }
                         
 function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("slideshow-img");
+    let i;
+    let x = document.getElementsByClassName("slideshow-img");
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length}
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";  
     }
+    //make the slide visible 
     x[slideIndex-1].style.display = "block";  
+
+    //set company name and description for that slide
+    document.getElementById('company-name').innerText = data[slideIndex-1].name;
+    document.getElementById('company-desc').innerText = data[slideIndex-1].desc;
+    //Not yet working
+    //document.getElementById('name-and-desc').classList.remove('fade');
+    //document.getElementById('name-and-desc').classList.add('fade');
 }
 
-/*
-    let index = 0;
-
-    //set company name
-    document.getElementById('company-name').innerText = data[index].name;
-    //set image
-    let imgNode = document.createElement('img');
-    //imgNode.src = data[index].src;
-    //imgNode.className = 'slideshow-img';
-    //document.getElementById('display-box').appendChild(node);
-
-
-/*
-const para = document.createElement("p");
-    const node = document.createTextNode("This is new.");
-    para.appendChild(node);
-    
-    const element = document.getElementById("div1");
-    element.appendChild(para);
-    
-    console.log("hi");
-    */
 
