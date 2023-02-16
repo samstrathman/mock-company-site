e4ae4a5118978a9a90b85dc9057a799fa2b9a8aa
samstrathman <117926965+samstrathman@users.noreply.github.com>
2023-02-16 11:38:29 -0500

    DONE I THINK

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-//Stuff for scroll animation
+//Stuff for scroll animations
 window.addEventListener(
     "scroll",
     () => {
+        //create the --scroll variable for the 3 rectangles that slide across the screen
         document.body.style.setProperty(
           "--scroll",
           window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+        );
+        //create the --scroll-fade variable for all the animations that handle the color shifts
+        document.body.style.setProperty(
+            "--scroll-fade",
+            (window.pageYOffset - document.getElementById('works-section').offsetHeight * 1.95) / (document.body.offsetHeight - window.innerHeight)
         );
     },
     false
 );
 
+//The rest of this code is for handling the elongating rectangle animation
 //listen for scroll event and call animate function
 document.addEventListener('scroll', animate);
 
-//Stuff to trigger animation when it's in view
 //get the element to animate
-var element = document.getElementById('ten');
-var elementHeight = element.clientHeight;
+let bar = document.getElementById('ten');
 
 //animate element when it is in view
 function animate() {
-    // is element in view?
-    if (inView()) {
+    if (inView(bar)) {
         // element is in view, add class to element
-        element.classList.add('elongate');
+        bar.classList.add('elongate');
     }
 }
 
-
 //check if element is in view
-function inView() {
+function inView(element) {
+    var elementHeight = element.clientHeight;
     // get window height
     var windowHeight = window.innerHeight;
     // get number of pixels that the document is scrolled
@@ -44,18 +48,5 @@ function inView() {
     if (scrollPosition > elementPosition) {
         return true;
     }
-  
     return false;
 }
-
-
-
-//animate the background color shift and logo and hamburger menu
-let worksSection = document.getElementById('works-section');
-//inView(worksSection).on('enter', function(el){
-//  var color = el.attributes('data-background-color');
-//  console.log(color);
-//})
-
-inView(worksSection).on('enter', console.log('hi'));
-
